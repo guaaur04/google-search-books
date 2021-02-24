64c69f1394f384ecc26ec42ae64d53a2f125aafc
V Guardado <auricia.guardado@gmail.com>
2021-02-24 01:44:22 -0800

    readme

---
+++ README.md
@@ -0,0 +1,17 @@
+
+## Description
+
+
+## Features
+Search, view, and sort employee data.
+
+## Screenshots and Demonstration
+
+
+## Technology
+React! Mongo Bootstrap 
+
+## License
+License: MIT
+
+Copyright (c) 2021 Auricia V Guardado
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "../components/Nav";
 import Hero from "../components/Hero";
+import { Input, FormBtn } from "../components/Form";
 
 function Search() {
   return (
@@ -8,6 +9,22 @@ function Search() {
         <Nav />
         <Hero />
       <h1>Search</h1>
+
+      <form>
+              <Input
+                // onChange={handleInputChange}
+                name="title"
+                placeholder="Book (required)"
+              />
+              <FormBtn
+                // onClick={handleFormSubmit}
+              >
+                Search
+              </FormBtn>
+            </form>
+
+
+
     </div>
   );
 }
